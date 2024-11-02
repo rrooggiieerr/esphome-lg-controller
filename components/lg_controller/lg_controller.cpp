f62e0064a8517703d5b6ad21aeee53f085acc4bd
Rogier van Staveren <rogier@batoid.com>
2024-11-02 04:42:16 +0100

    Only set entity internal configuration when entities are available

--- components/lg_controller/lg_controller.cpp
+++ components/lg_controller/lg_controller.cpp
@@ -169,8 +169,10 @@ void LGControllerComponent::configure_capabilities() {
 			}
 		}
 
-		this->auto_dry_entity_->set_internal(!this->parse_capability(LgCapability::AUTO_DRY));
-		this->auto_dry_active_entity_->set_internal(!this->parse_capability(LgCapability::AUTO_DRY));
+		if (this->auto_dry_entity_ != nullptr)
+			this->auto_dry_entity_->set_internal(!this->parse_capability(LgCapability::AUTO_DRY));
+		if (this->auto_dry_active_entity_ != nullptr)
+			this->auto_dry_active_entity_->set_internal(!this->parse_capability(LgCapability::AUTO_DRY));
 	}
 
 	this->internal_thermistor_ = this->slave_;
